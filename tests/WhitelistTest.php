b42cad31fb5fbe4e62a5c1855c5d5ae072bb991c
Brett Scarlett <brett@scarletttech.com>
2024-08-07 15:21:19 -0400

    Unit Tests and other small additions (may add to ignore).

--- tests/WhitelistTest.php
+++ tests/WhitelistTest.php
@@ -1,19 +1,56 @@
 <?php
 
-class WhitelistTest extends \PHPUnit\Framework\TestCase {
-    public function TestQueryMojangApiSuccess() {
+use PHPUnit\Framework\TestCase;
+use src\Whitelist;
+
+// IMPORTANT - These unit tests, especially creating the Whitelist and destroying it are in conjunction with each-other. Running them separately will cause issues.
+
+class WhitelistTest extends TestCase {
+
+    public function testQueryMojangApiSuccess() {
         
         $whitelist = new Whitelist;
-        $userInfo = $whitelist->getUserDetails("Duality13011");
-        $this->assertEquals(0, (empty($userInfo)));
+        $user = $whitelist->getUserDetails("Duality13011");
+        $this->assertEquals(0, (empty($user)));
         
     }
-    public function TestQueryMojangApiFailure() {
-        
+    public function testQueryMojangApiFailure() {
+
         $whitelist = new Whitelist;
-        $userInfo = $whitelist->getUserDetails("TestUsernamethatwillneverequaltotrue$12399");
-        $this->assertEquals(0, $userInfo);
-        
+        $user = $whitelist->getUserDetails("TestUsernamethatwillneverequaltotrue$12399");
+        $this->assertEquals(0, $user);
+
+    }
+
+    public function testWhiteListAddition(){
+        $whitelist = new Whitelist;
+
+        $user = $whitelist->getUserDetails("Duality13011");
+        $whitelist->addUserToWhitelist($user);
+
+        $user2 = $whitelist->getUserDetails("13011brett");
+        $whitelist->addUserToWhitelist($user2);
+
+        $this->assertTrue(
+            $whitelist->queryWhitelistForUser($user)
+            && $whitelist->queryWhitelistForUser($user2));
+    }
+    public function testWhiteListRemovalByNameAndUUID(){
+        $whitelist = new Whitelist;
+
+        $user = "duality13011";
+        $whitelist->deleteUserFromWhitelist($user);
+
+        $user2 = "9b579140a41341fb99b1e1dfb3007023";
+        $whitelist->deleteUserFromWhitelist($user2);
+
+        $this->assertFalse(
+            $whitelist->queryWhitelistForUser($user)
+            && $whitelist->queryWhitelistForUser($user2));
+
+        if(file_exists('whitelist.json')) unlink('whitelist.json');
+
+
     }
 
 
